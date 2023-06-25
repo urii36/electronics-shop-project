8fd74c9fe2de6ac0f3476d046e48d6385aac9f63
urii36 <kazan313131@gmail.com>
2023-06-26 00:57:27 +0300

    Home work1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +15,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,34 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+@pytest.fixture
+def item1():
+    item = Item("Ноутбук", 10000, 20)
+    return item
+def test_item1_name(item1):
+    assert item1.name == 'Ноутбук'
+
+
+def test_item1_name_change(item1):
+    item1.name = 'Смартфон'
+    assert item1.name == 'Смартфон'
+
+
+def test_item1_price(item1):
+    assert item1.price == 10000
+
+
+def test_item1_quantity(item1):
+    assert item1.quantity == 20
+
+
+def test_item1_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
+
+def test_item1_discount(item1):
+    item1.pay_rate = 0.9
+    item1.apply_discount()
+    assert item1.price == 9000
+
