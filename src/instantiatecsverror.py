239e301a647378a01eaccc75ab64b0345b891ed3
urii36 <135154938+urii36@users.noreply.github.com>
2023-06-26 22:30:33 +0300

    Delete instantiatecsverror.py
    не нужный файл2

--- src/instantiatecsverror.py
+++
@@ -1,8 +0,0 @@
-class InstantiateCSVError(Exception):
-    """
-    Исключение, выбрасываемое при ошибке чтения CSV-файла.
-    """
-
-    def __init__(self, message: str = "Файл item.csv поврежден"):
-        super().__init__(message)
-        self.message = message
