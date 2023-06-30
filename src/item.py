958b5162e3d51a0733eac9df3806e205f1f18ad2
urii36 <kazan313131@gmail.com>
2023-06-30 14:09:42 +0300

    Home work 2 and 3

--- src/item.py
+++ src/item.py
@@ -1,5 +1,6 @@
 import csv
 
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,16 +10,51 @@ class Item:
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
+        экземпляра класса item.
         """
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+        self.__name = name
+        self.__price = price
+        self.__quantity = quantity
         self.all.append(self)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}{self.name, self.price, self.__quantity}'
+
+    def __str__(self):
+        return self.__name
+
+    @property
+    def name(self) -> str:
+        """
+        Название товара.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value) -> None:
+        """
+        Устанавливает название товара.
+        Если длина названия превышает 10 символов, обрезает его до первых 10 символов.
+        :param value: Новое название товара.
+        """
+        self.__name = value if len(value) <= 10 else value[:10]
+
+    @property
+    def price(self) -> float:
+        """
+        Получает количество товара в магазине.
+
+        """
+        return self.__price
+
+    @property
+    def quantity(self) -> int:
+        """
+        Получает цену за единицу товара.
+
+        :return: Цена за единицу товара.
+        """
+        return self.__quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -26,11 +62,28 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        return self.price * self.__quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= self.pay_rate
+        self.__price *= self.pay_rate
 
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """
+        Преобразует числовую строку в число.
+
+        :param value: Числовая строка.
+        :return: Преобразованное число.
+        """
+        return int(float(value))
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        with open('../src/items.csv', 'r', encoding='windows-1251') as file:
+            file_reader = csv.DictReader(file)
+            for row in file_reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,18 +1,69 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-from src.item import Item
 import pytest
 
+from src.item import Item
+
+
+@pytest.fixture(autouse=True)
+def clear_items():
+    Item.all = []
+
+
+@pytest.mark.parametrize(
+    ['price', 'quantity', 'total_price'], (
+            (10_000, 20, 200_000),
+            (1_000, 5, 5_000),
+            (1_000, 0, 0),
+    )
+)
+def test_calculate_total_price(price, quantity, total_price):
+    item = Item('test_item', price, quantity)
+    assert item.calculate_total_price() == total_price
+
+
+@pytest.mark.parametrize(
+    ['discount', 'price_with_discount'], (
+            (0, 10_000),
+            (0.15, 8_500),
+            (0.27, 7_300),
+    )
+)
+def test_apply_discount(discount, price_with_discount):
+    item = Item('test_item', 10_000, 20)
+    Item.pay_rate = 1 - discount
+
+    item.apply_discount()
+
+    assert item.price == price_with_discount
+
+
+def test_all_items():
+    item1 = Item('test_1', 10_000, 20)
+    item2 = Item('test_2', 20_000, 5)
+
+    assert Item.all == [item1, item2]
+
+
+def test_instantiate_from_csv():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+
+    assert Item.string_to_number('5.5') == 5
+
+
+def string_to_number(value: str):
+    assert value == 5
+
+
 @pytest.fixture
-def item1():
-    item = Item("Ноутбук", 10000, 20)
-    return item
-def test_item1_name(item1):
-    assert item1.name == 'Ноутбук'
+def test_item1_repr(item1):
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
 
 
-def test_item1_name_change(item1):
-    item1.name = 'Смартфон'
-    assert item1.name == 'Смартфон'
+@pytest.fixture
+def test_item1_str(item1):
+
+    assert str(item1) == 'Смартфон'
 
 
 def test_item1_price(item1):
@@ -31,4 +82,14 @@ def test_item1_discount(item1):
     item1.pay_rate = 0.9
     item1.apply_discount()
     assert item1.price == 9000
+
+def test_item_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+def test_item_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('10.0') == 10
+    assert Item.string_to_number('10.5') == 10
+
 
